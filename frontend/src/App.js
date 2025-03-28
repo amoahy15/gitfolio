ceedb65af751f1e906f81a8ecf093458a2351be4
Humphrey Amoakohene <amoakohene.humphrey15@gmail.com>
2025-03-28 18:50:10 -0400

    Initiial commit includes react app setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Background from './components/Background';// Note: Adjusted path to match your file structure
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Background />
+    </div>
+  );
+}
+
+export default App;
